99231b1a66816daa80e47f7ee7067591d3e55279
JoaBerra <31992783+JoaBerra@users.noreply.github.com>
2023-01-19 19:27:27 +0100

    initial commit

---
+++ deploy/01-deploy-fund-me.js
@@ -0,0 +1,52 @@
+// async function deployFunc() {
+//     console.log("Hi!");
+// }
+
+// module.exports.default = deployFunc;
+
+// module.exports = async (hre) => {
+//     const {getnamedAccounts, deployments} = hre
+// }    --> Same thing as hre.getNamedAccounts
+//      -->               hre.deployments
+
+const {
+    networkConfig,
+    developmentChains,
+} = require("../helper-hardhat-config.js")
+const { network } = require("hardhat")
+const { verify } = require("../utils/verify.js")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    let ethUsdPriceFeedAddress
+    if (developmentChains.includes(network.name)) {
+        const ethUsdAggregator = await deployments.get("MockV3Aggregator")
+        ethUsdPriceFeedAddress = ethUsdAggregator.address
+    } else {
+        ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    }
+
+    // if the contract doesn't exist, we deploy a minimal version of it
+    // for our local testing
+
+    // well what happens when
+    // when going for localhost or hardhat network we want to use a mock
+    const fundMe = await deploy("FundMe", {
+        from: deployer,
+        args: [ethUsdPriceFeedAddress], // put price feed address
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        await verify(fundMe.address, [ethUsdPriceFeedAddress])
+    }
+    log("______________________________")
+}
+module.exports.tags = ["all", "fundme"]
